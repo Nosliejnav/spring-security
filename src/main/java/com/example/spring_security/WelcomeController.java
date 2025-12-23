8601d43cf6a7deddd535a578bb9588ff388fcda2
Nosliejnav <vanjeilsonm10@gmail.com>
2025-12-22 22:19:02 -0300

    Autenticação simples - Testanco com uma controler

---
+++ src/main/java/com/example/spring_security/WelcomeController.java
@@ -0,0 +1,27 @@
+package com.example.spring_security;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Bem-vindo ao Spring Security!";
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasAnyRole('MANAGERS', 'USERS')")
+    public String users() {
+        return "Bem-vindo, usuário!";
+    }
+
+    @GetMapping("/managers")
+    @PreAuthorize("hasRole('MANAGERS')")
+    public String managers() {
+        return "Bem-vindo, gerente!";
+    }
+}
+
